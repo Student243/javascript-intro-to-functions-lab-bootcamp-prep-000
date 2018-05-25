f88275971f3d8963b095cf016a7f110e1ad06c0c
Kevin Andrew Johnson <kjohnson5315@hotmail.com>
2018-05-25 00:52:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
   return `YES INDEED!`
   }
-  if (string.ILoveYouGrandma() === string) {
-  return "I love you, too"
+  if ("I love you, Grandma." === string) {
+  return "I love you, too."
   }
 } 
